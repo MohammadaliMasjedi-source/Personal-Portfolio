9de4e9dedb1b2cb1e1c54d0860f3cc6522a7ed5a
Mohammadali Masjedi <mohammadalimasjedi@gmail.com>
2026-06-06 07:37:50 +0200

    sync: routine commit 2026-06-06 07:37

---
+++ MC-Dashboard.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Mo ? MassJedi ? Portfolio - opens its dashboard (Mission Control, focused on this project)
+start "" "file:///D:/Mo.MassJedi/Git%20Mo/Mission%20Control/index.html#p=massjedi-website"
